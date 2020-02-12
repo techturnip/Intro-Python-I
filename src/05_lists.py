9dee0b0d5749dccd2936d3e0976bbdc8babbaf7f
Tyler Turnipseed <tyler@techturnip.us>
2020-02-12 17:11:57 -0500

    completed 05_lists.py

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,35 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+for v in y:
+    x.append(v)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
+# for v in x:
+#     if v == 8:
+#         x.remove(v)
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+for (i, v) in enumerate(x):
+    if v == 9:
+        x.insert(i + 1, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for v in x:
+    print(v * 1000)
